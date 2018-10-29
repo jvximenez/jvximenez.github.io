8097de5fc2bf2f0d764b71a457533fd4778508b6
Joao Vitor Ximenez <jvximenez@gmail.com>
2018-10-29 02:36:36 +0100

    colocando graficos

--- src/pages/cards-do-mes/cards-do-mes.ts
+++ src/pages/cards-do-mes/cards-do-mes.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FirebaseServiceProvider } from '../../providers/firebase-service/firebase-service';
 import { EdicaoPage } from '../edicao/edicao';
+import chartJs from 'chart.js';
 
 
 /**
@@ -13,10 +14,17 @@ import { EdicaoPage } from '../edicao/edicao';
 
 @IonicPage()
 @Component({
+
+  
   selector: 'page-cards-do-mes',
   templateUrl: 'cards-do-mes.html',
 })
 export class CardsDoMesPage {
+  @ViewChild('barCanvas') barCanvas;
+  barChart: any;
+  @ViewChild('pieCanvas') pieCanvas;
+  pieChart: any;
+
   public mes: any
   public dias: any
   public verdade: boolean
@@ -25,11 +33,20 @@ export class CardsDoMesPage {
   public passos;tempinho;quantidade;UHU;cafe;lancheM;almoco;lancheT;jantar;estudos;aulas
   public refrigerante; alcool;leitura;agradecimento
 
+  public graficoG;graficoData;graficoDado
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: FirebaseServiceProvider) {
+
+    
+
     this.mes = this.navParams.get('mes');
     this.verdade = true;
-
     this.dias = this.dbService.getAllEspecifico('diario','parcial',String(this.mes)).map(a => a.reverse());
+
+    this.graficoG = this.Media2('passos');
+    this.graficoData = this.graficoG[0];
+    this.graficoDado = this.graficoG[1];
+    console.log("aqui",this.graficoG,this.graficoDado,this.graficoData)
 
     this.passos = this.Media("passos");
     this.tempinho = this.Media("tempinho");
@@ -85,17 +102,27 @@ export class CardsDoMesPage {
   }
 
 
+  Media2(dado){
+    let array = []
+    let array2 = []
+    let array3 = []
+    let final : number;
+    this.dias.forEach(element => {element.forEach(dia => {(array).push(Number(dia[dado])), (array2).push(Number(dia['dia']))})
+    console.log(array, array2)})
+    array3 = [array,array2]
+    console.log("3", array3)
+    return array3
+  }
+
   Media(dado){
     let soma = 0
     let quantidade = 0
     let array: number[] = []
     let final : number;
-    this.dias.forEach(element => {element.forEach(dia => {console.log(dia),quantidade += 1 ,  soma += Number(dia[dado]), console.log(soma, quantidade) }),
-    console.log("final", quantidade,soma),
+    this.dias.forEach(element => {element.forEach(dia => {quantidade += 1 ,  soma += Number(dia[dado]) }),
     final = Number(soma)/Number(quantidade),
     final =  Math.round(final)
-    console.log("final2", final), array.push(final)})
-    console.log("final3",array)
+    array.push(final)})
     return array;
   }
 
@@ -104,21 +131,65 @@ export class CardsDoMesPage {
     let quantidade = 0
     let array: number[] = []
     let final : number;
-    this.dias.forEach(element => {element.forEach(dia => {console.log(dia),quantidade += 1 ; if (dia[dado] > 0 || dia[dado] == true)  {soma += 1} ; console.log(soma, quantidade) }),
-    console.log("final", quantidade,soma),
+    this.dias.forEach(element => {element.forEach(dia => {quantidade += 1 ; if (dia[dado] > 0 || dia[dado] == true)  {soma += 1} }),
+    
     final = (Number(soma)/Number(quantidade))*100,
     final =  Math.round(final)
-    console.log("final2", final), array.push(final)})
-    console.log("final3",array)
+    array.push(final)})
+    
     return array;
 
   }
 
+  //--------------------CHarts-------------------------------//
+
+  ngAfterViewInit(){
+    setTimeout(()=> {
+      this.barChart = this.getBarChart();
+      
+    },100)
+   }
+
+    getBarChart(){
+      const data = {
+        labels: ['Passos',"dsfdfs","ddsdas"],
+        datasets: [{
+          label: ['passos'],
+          data: [151,1516,545],
+          backgroundColor:  '#2f6acf',
+          borderWidth: 2
+      }]
+    
+    };
   
+    const options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            autoSkip: false,
+          }
+        }],
+        xAxes: [{
+          ticks: {
+            autoSkip: false,
+          }
+  
+        }]
+      }
+    }
+    
+  
+    return this.getChart(this.barCanvas.nativeElement, 'bar', data, options);
+  }
+  
+    getChart(context, charType, data, options){
+      return new chartJs(context, {
+        data,
+        options,
+        type: charType
+      })
+    }
 
-
-
-
- 
 
 }
