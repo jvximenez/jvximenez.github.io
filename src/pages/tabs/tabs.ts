e7911b1290b2a9a1b58bcd322b92f3a4fdefcbd9
Joao Vitor Ximenez <jvximenez@gmail.com>
2018-11-22 16:25:09 +0100

    apenas alguns testes nas tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,18 +2,26 @@ import { Component } from '@angular/core';
 import { AdicionarPage } from '../adicionar/adicionar';
 import { HojePage } from '../hoje/hoje';
 import { CardsPage } from '../cards/cards';
+import { TarefasPage } from '../tarefas/tarefas';
+import { NavParams } from 'ionic-angular';
 
 
 @Component({
   templateUrl: 'tabs.html'
+  
 })
 export class TabsPage {
+  myIndex
 
   tab1Root = AdicionarPage;
   tab2Root = HojePage;
   tab3Root = CardsPage;
+  tab4Root = TarefasPage;
 
-  constructor() {
+  constructor( navParams: NavParams) {
+    this.myIndex = 0;
+    if (navParams.data.index) this.myIndex = navParams.data.index;
+}
 
-  }
+  
 }
