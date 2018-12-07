f0da84e3059392f72df377a66ff706800ea57005
Joao Vitor Ximenez <jvximenez@gmail.com>
2018-12-07 21:39:08 +0100

    quase terminando configs

--- src/pages/configuracoes/configuracoes.ts
+++ src/pages/configuracoes/configuracoes.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { FirebaseServiceProvider } from '../../providers/firebase-service/firebase-service';
 
 /**
  * Generated class for the ConfiguracoesPage page.
@@ -14,12 +15,110 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'configuracoes.html',
 })
 export class ConfiguracoesPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public tarefa = {
+    'title':'',
+    'nivel':'',
+    'check':true,
+    'categoria':'',
+    'ordem':'',
+  }
+  public remedio = {
+    'title':'',
+    'check':true,
+    'ordem':'',
+  }
+  public atividade = {
+    'title':'',
+    'check':true,
+    'ordem':'',
+  }
+  public role = {
+    'title':'',
+    'check':true,
+    'ordem':'',
+  }
+  public estudo = {
+    'title':'',
+    'check':true,
+    'ordem':'',
+  }
+  public pessoa = {
+    'title':'',
+    'check':true,
+    'ordem':'',
+  }
+  public viagem= {
+    'pais':'',
+    'check':true,
+  }
+  public cidade = {
+    'title':'',
+    'check':true,
+  }
+  public dia = {
+    'title':'',
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ConfiguracoesPage');
+  public tarefas; remedios; atividades; roles; estudos; pessoas; viagens; cidades; dias;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: FirebaseServiceProvider, public alertCtrl: AlertController) {
+    this.remedios=this.dbService.getAll('configuracoes/remedios','ordem')
+
   }
+
+
+  CriaNovo(local){
+    const prompt = this.alertCtrl.create({
+      title: 'Novo:'+String(local),
+      message: "Entre com o nome e ordem",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Nome',
+
+        },
+        {
+          name: 'ordem',
+          placeholder:"Ordem"
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Adicionar',
+          handler: data => {
+            if (local == "remedios"){
+              this.remedio.title = data.title, this.remedio.ordem = data.ordem;
+              this.dbService.save('configuracoes/remedios', this.remedio);
+            }
+            
+            if (local == "atividades") {
+              this.atividade.title = data.title, this.atividade.ordem = data.ordem;
+              this.dbService.save('configuracoes/atividades', this.atividade);
+            }
+            if (local == "roles") {
+              this.role.title = data.title, this.role.ordem = data.ordem;
+              this.dbService.save('configuracoes/atividades', this.role);
+            }
+            if (local == "estudos") {
+              this.estudo.title = data.title, this.estudo.ordem = data.ordem;
+              this.dbService.save('configuracoes/estudos', this.estudo);
+            }
+            if (local == "pessoas") {
+              this.pessoa.title = data.title, this.pessoa.ordem = data.ordem;
+              this.dbService.save('configuracoes/pessoas', this.pessoa);
+            }
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+
+
+  
 
 }
