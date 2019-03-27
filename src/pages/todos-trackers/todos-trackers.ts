5ac3534b6be517f92fc66f025896ab943daf920e
Joao Vitor Ximenez <jvximenez@gmail.com>
2019-03-27 14:35:44 -0300

    alterando pagina de tracker especifico

--- src/pages/todos-trackers/todos-trackers.ts
+++ src/pages/todos-trackers/todos-trackers.ts
@@ -21,9 +21,17 @@ export class TodosTrackersPage {
   public Dias
   public DiasValores 
   public teste: number = 20
+  public ShowResumo = false
+
+  public testeL = 1
+
+  public trackers2
+  public arrayOb
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: FirebaseServiceProvider) {
     this.trackers = this.dbService.getAll('trackers','total').map(a => a.reverse())
+    this.trackers2 = this.trackers
+    this.arrayOb = this.CriaArrayObjetos()
     this.Dias = this.GetArrayDias()
     
   
@@ -77,6 +85,95 @@ export class TodosTrackersPage {
       {'trackers' : this.trackers,
     'total': total})
     }
+
+  TotalToData(total){
+    var dia;
+    var mes;
+    var ano;
+    var semana
+    var final;
+    ano = total.substr(0,4);
+    mes = total.substr(4,2);
+    dia = total.substr(6,2);
+
+    var myDate = new Date();
+    myDate.setFullYear(Number(ano));
+    myDate.setMonth(Number(mes)-1);
+    myDate.setDate(Number(dia)); 
+    if(myDate.getDay() == 0){semana = 'Segunda'}
+    if(myDate.getDay() == 1){semana = 'Terça'}
+    if(myDate.getDay() == 2){semana = 'Quarta'}
+    if(myDate.getDay() == 3){semana = 'Quinta'}
+    if(myDate.getDay() == 4){semana = 'Sexta'}
+    if(myDate.getDay() == 5){semana = 'Sábado'}
+    if(myDate.getDay() == 6){semana = 'Domingo'}
+
+    final = (dia+'/'+mes+'/'+ano+" - "+semana)
+    return (final)
+  }
+
+
+  GetSomaCategoria(Total,Parametro){
+
+  }
+
+  VerMais(){
+    this.ShowResumo = !this.ShowResumo
+  }
+
+  VerMaisOuMenos(){
+    if (this.ShowResumo == true){
+      return ('Ver menos')
+    }
+    else{return 'Ver mais'}
+  }
+
+
+    ChecaValor3(parametro1,parametro2){
+      var array = 0
+      
+      this.arrayOb.forEach(itens => {itens.forEach(item => {if (item['total'] == parametro1){
+        if(item['title'].includes(parametro2)){array += Number(item['duracao'])}
+       }}) })
+      
+      return(array)
+   
+    }
+
+  CriaArrayObjetos(){
+    var array = []
+    this.trackers2.forEach(element => {array.push(element)})
+    
+    return array
+  }
+
+
+  Arredonda(val){
+    var b;
+    b = (Math.round(val*100)/100)
+    return b
+  }
+
+  RetornaH(duracao){
+    if (duracao == 0){
+      return (" ")
+    }
+    else{
+    var min2
+    var inteiro = parseInt(duracao)
+    var min = Math.round((duracao - inteiro)*60)
+    min2 = String(min)
+    if (min < 10) {min2 = String("0"+min)}
+    return (inteiro+"h"+min2)}
+  }
+
+
+  
+
+  
+
+  
+  
   
 
 }
--- src/pages/tracker-especifico/tracker-especifico.ts
+++ src/pages/tracker-especifico/tracker-especifico.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { FirebaseServiceProvider } from '../../providers/firebase-service/firebase-service';
+import { TimeTrackerEditPage } from '../time-tracker-edit/time-tracker-edit';
 
 /**
  * Generated class for the TrackerEspecificoPage page.
@@ -16,11 +18,54 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class TrackerEspecificoPage {
   public total
   public trackers
+  public Indicadores;IndicadoresO
+  input ={
+    'title':''
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  tracker = {
+    'dia':'',
+    'mes':'',
+    'ano':'',
+    'total':'',
+    'parcial':'',
+    'title':'',
+    'Hinicio':Number(''),
+    'Minicio':Number(''),
+    'Hfim':'',
+    'Mfim':'',
+    'nivel': 0,
+    'duracao':0,
+    'check': false,
+
+  }
+  actionSheetControler: any;
+  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: FirebaseServiceProvider, public alertCtrl: AlertController) {
     this.total = this.navParams.get('total')
     this.trackers =  this.navParams.get('trackers')
+    
   }
+
+  GetHora(){
+    var a = new Date
+    var b
+    b = a.getHours() + a.getMinutes()/60
+    b = b/24
+    console.log(b)
+    return (b)
+
+  }
+
+
+  
+
+  ChangeCheckTarefas(tarefa){
+    tarefa.check = !tarefa.check
+    this.dbService.update('configuracoes/shows',tarefa)
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TrackerEspecificoPage');
@@ -59,18 +104,347 @@ export class TrackerEspecificoPage {
     }
   }
 
-  RetornaH(duracao){
-    if (duracao == 0){
-      return (" ")
+
+
+  CorIfTrue(dado){
+    if (dado ==  true){
+      return "primary"
     }
-    else{
-    var min2
-    var inteiro = parseInt(duracao)
-    var min = Math.round((duracao - inteiro)*60)
-    min2 = String(min)
-    if (min < 10) {min2 = String("0"+min)}
-    return (inteiro+"h"+min2)}
+    if (dado !=  true){
+      return ""
+    }
   }
+
+  AlteraNivel(track,Nivel){
+    track.nivel = Nivel
+    this.dbService.update('trackers',track)
+
+  }
+
+  Nivel(Nivel){
+    this.tracker.nivel = Nivel
+
+  }
+
+  Criacao(tarefa){
+    this.tracker.title = this.input.title
+    this.tracker.dia = this.total.substr(6,2)
+    this.tracker.mes = this.total.substr(4,2)
+    this.tracker.ano = this.total.substr(0,4)
+    this.tracker.total = this.total
+    this.tracker.parcial =  this.total.substr(0,4)+this.total.substr(4,2)
+    this.dbService.save('trackers',tarefa)
+    this.input.title = ""
+    this.tracker.Hinicio = Number('')
+    this.tracker.Minicio = Number('')
+
+  }
+
+  goToEdit(itens){
+    this.navCtrl.push(TimeTrackerEditPage, 
+    {'tracker' : itens})
+  }
+
+  Atualizar(tarefas){
+    this.dbService.update('trackers',tarefas)
+    }
+  
+  Deletar(tarefas){
+    this.dbService.revome('trackers',tarefas)
+    }
+
+    RetornaH(duracao){
+      if (duracao == 0){
+        return (" ")
+      }
+      else{
+      var min2
+      var inteiro = parseInt(duracao)
+      var min = Math.round((duracao - inteiro)*60)
+      min2 = String(min)
+      if (min < 10) {min2 = String("0"+min)}
+      return (inteiro+"h"+min2)}
+    }
+  
+  
+    Comecar(track){
+      
+      if (track.Minicio != '' && track.Hfim == ''){
+        track.Hfim = this.RetornaHora()
+        track.Mfim = Math.round(this.RetornaMin()*100)/100
+        track.check = true
+        track.duracao = (Math.round((Number(track.Hfim) + Math.round((Number(track.Mfim)/60)*10000)/10000 - Number(track.Hinicio) - Math.round((Number(track.Minicio)/60)*10000)/10000)*10000)/10000)
+  
+      if (Number(track.duracao) <= 0)
+        {track.duracao = 24 - Number(track.Hinicio)- Number(track.Minicio)/60}
+      } 
+      if (track.Minicio == ''){
+        track.Hinicio = this.RetornaHora()
+        track.Minicio = Math.round(this.RetornaMin()*100)/100}
+        this.dbService.update('trackers',track)
+       
+  
+    }
+  
+    
+  
+    RetornaHora(){
+      var data = new Date();
+      var hora = data.getHours();
+      return (hora)
+     
+    }
+    RetornaMin(){
+      var data = new Date();
+      var min = data.getMinutes();
+      var segundo = data.getSeconds();
+      return (min + segundo/60)
+     
+    }
+
+    CriaNovo(track){
+      this.tracker.Hinicio = track.Hfim
+      this.tracker.Minicio = track.Mfim
+      this.Criacao(this.tracker);
+    }
+
+
+    Opcoes(track){
+      const actionSheet = this.actionSheetControler.create({
+        title: 'Opções',
+        buttons: [
+          {
+            text: 'Copiar',
+            
+            handler: () => {
+              this.input.title = track.title;
+              this.tracker.nivel = track.nivel;
+              this.Criacao(this.tracker);
+            }
+            
+          },
+          {
+            text: 'Deletar',
+            
+            handler: () => {
+              this.Deletar(track);
+            }
+            
+          },{
+            text: 'Limpar final',
+            handler: () => {
+              track.Hfim = '';
+              track.Mfim = '';
+              this.dbService.update('trackers',track);
+            }
+          },{
+            text:"Alterar Horários",
+            handler:() => {this.AlteraHora(track)}
+          },{
+            text: 'Limpar tudo',
+            handler: () => {
+              track.Hfim = '';
+              track.Mfim = '';
+              track.Hinicio = '';
+              track.Minicio = '';
+              track.duracao = '';
+              this.dbService.update('trackers',track);
+              
+            }
+          },{
+            text: 'Alterar Nível',
+            handler: () => {
+              this.AlteraNivelradio(track);
+            }
+          },{
+            text: 'Relaxar',
+            handler: () => {
+              this.AlteraNivel(track,"-2");
+              this.AlterarNome(track, 'Relaxar');
+            }
+          },{
+            text: 'Criar Novo',
+              handler: () => {
+              this.PromptCriarNovo(track)
+             
+            }
+          },{
+            text: 'Cancelar',
+            role: 'cancel',
+            handler: () => {
+            }
+          }
+        ]
+      });
+      actionSheet.present();
+    }
+
+
+    AlterarNome(track, nome){
+      track.title = nome;
+      this.dbService.update('trackers',track)
+  
+    }
+
+
+
+    PromptCriarNovo(track){
+      const prompt = this.alertCtrl.create({
+        title: 'Novo tracker',
+        inputs: [
+          {
+            name: 'title',
+            placeholder: 'Nome'
+          },{
+            name: 'nivel',
+            placeholder:'Nível'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancelar',
+            handler: data => {
+              console.log('Cancel clicked');
+            }
+          },
+          {
+            text: 'Relaxar',
+            handler: data => {
+              this.input.title = "Relaxar", this.tracker.nivel = -2;
+              this.CriaNovo(track)
+              
+            }
+          },
+          {
+            text: 'Salvar',
+            handler: data => {
+              this.input.title = data.title, this.tracker.nivel = data.nivel;
+              this.CriaNovo(track)
+            }
+          }
+        ]
+      });
+      prompt.present();
+    }
+  
+  
+    Atual(track){
+      if (track.Minicio != '' && track.Mfim == ''){
+        var Hfim = this.RetornaHora()
+        var Mfim = Math.round(this.RetornaMin()*100)/100
+        var dura = (Math.round((Number(Hfim) + Math.round((Number(Mfim)/60)*10000)/10000 - Number(track.Hinicio) - Math.round((Number(track.Minicio)/60)*10000)/10000)*10000)/10000)
+        return (dura);
+      } 
+      if (track.Duracao != ''){
+        return(" ")
+  
+      } 
+    }
+    
+
+    AlteraHora(track){
+      const prompt = this.alertCtrl.create({
+        title: 'Hora',
+        message: "Entre com a hora",
+        inputs: [
+          {
+            name: 'hora1',
+            placeholder: track.Hinicio,
+            type: 'number',
+          },
+          {
+            name: 'hora2',
+            placeholder: track.Hfim,
+            type: 'number',
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            handler: data => {
+              console.log('Cancel clicked');
+  
+            }
+          },
+          {
+            text: 'Save',
+            handler: data => {
+              track.Hinicio = data.hora1.split('.')[0];
+              track.Hfim = data.hora2.split('.')[0];
+              track.Minicio = data.hora1.split('.')[1];
+              track.Mfim = data.hora2.split('.')[1];
+  
+              if (track.Mfim > 0){
+              var dura = (Math.round((Number(track.Hfim) + Math.round((Number(track.Mfim)/60)*10000)/10000 - Number(track.Hinicio) - Math.round((Number(track.Minicio)/60)*10000)/10000)*10000)/10000)
+              track.duracao = dura;
+              track.check = true
+            }
+              
+              this.dbService.update('trackers',track)
+             
+            }
+          }
+        ]
+      });
+      prompt.present();
+    }
+
+
+
+    AlteraNivelradio(track){
+      let prompt = this.alertCtrl.create({
+        title: 'Alterando Nível',
+        message: 'Selecione a opção',
+        inputs : [
+        {
+            type:'radio',
+            label:'Nível 2',
+            value:'2'
+        },
+        {
+          type:'radio',
+          label:'Nível 1',
+          value:'1'
+      },
+        {
+          type:'radio',
+          label:'Nível 0',
+          value:'0'
+        },  
+        {
+          type:'radio',
+          label:'Nível -1',
+          value:'-1'
+      },
+      {
+        type:'radio',
+        label:'Nível -2',
+        value:'-2'
+      },
+      {
+        type:'radio',
+        label:'Necessario',
+        value:'3'
+      }],
+        buttons : [
+        {
+            text: "Cancel",
+            handler: data => {
+            console.log("cancel clicked");
+            }
+        },
+        {
+            text: "Salvar",
+            handler: data => {
+              track.nivel = data
+            this.dbService.update('trackers',track)
+            
+            }
+        }]});
+        prompt.present();
+    }
+
+
 
   
   
